c6fb3ca5d79c8cb1a4a5a6b58e4a55825c6d7187
VVasilevich <vve913@gmail.com>
2023-10-07 22:46:50 +0600

    Update showInfo

--- dist/script.js
+++ dist/script.js
@@ -237,6 +237,7 @@ class ShowInfo {
     this.btns.forEach(btn => {
       btn.addEventListener('click', () => {
         const sibling = btn.closest('.module__info-show').nextElementSibling;
+        sibling.classList.add('animated', 'fadeIn');
         sibling.classList.toggle('msg');
         sibling.style.marginTop = '20px';
       });
